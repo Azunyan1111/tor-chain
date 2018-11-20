2f9c5a1c7eeac76364591b0ea2aa79678b445a31
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2018-11-20 13:18:52 +0900

    update

--- app/pkttools-1.16/analyze.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PKTTOOLS_ANALYZE_H_INCLUDED_
-#define _PKTTOOLS_ANALYZE_H_INCLUDED_
-
-struct timeval;
-int pkt_analyze(FILE *fp, char *buffer, int size, int linktype,
-		struct timeval *tm, int low_layer, int high_layer, int level);
-
-#endif
--- app/pkttools-1.16/correct.h
+++
@@ -1,6 +0,0 @@
-#ifndef _PKTTOOLS_CORRECT_H_INCLUDED_
-#define _PKTTOOLS_CORRECT_H_INCLUDED_
-
-int pkt_correct(char *buffer, int size, int linktype);
-
-#endif
